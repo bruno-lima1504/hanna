6468efcc0638474d8352348e1cf98ba24bb901f3
Bruno Lima <bruno.lima1504@gmail.com>
2024-09-30 12:36:16 -0300

    feat: adds toast message

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
-import { registerRootComponent } from "expo";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native"; // Adicione essa linha
+import { registerRootComponent } from "expo";
+import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
-
+import Toast from "react-native-toast-message";
 import { AuthProvider } from "./src/contexts/AuthContext";
+import toastConfig from "./src/toastconfig";
 
 function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
         <Routes />
+        <Toast config={toastConfig} />
       </AuthProvider>
     </NavigationContainer>
   );
